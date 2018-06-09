041d08a4b29e8209eb7ca31446fe730e3285eeb9
embluk <embluk@tuta.io>
2018-06-09 18:40:40 +0100

    Add the basic starting point for starting a Phase server

---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = new swoole_http_server("127.0.0.1", 9501);
+
+$server->on("start", function ($server)
+{
+    echo "Swoole http server is started at http://127.0.0.1:9501\n";
+});
+
+$server->on("request", function ($request, $response)
+{
+    $response->header("Content-Type", "text/plain");
+    $response->end("Hello World\n");
+});
+
+$server->start();
