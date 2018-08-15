120eff2d654568437965e08994f82ce69a9c0042
embluk <embluk@tuta.io>
2018-08-16 00:42:48 +0200

    Remove space after function keywords

--- server.php
+++ server.php
@@ -6,12 +6,12 @@ require __DIR__ . "/core/autoload.php";
 
 $server = new swoole_http_server("192.168.0.19", 9501);
 
-$server->on("start", function ($server)
+$server->on("start", function($server)
 {
     echo "Swoole http server is started at http://127.0.0.1:9501\n";
 });
 
-$server->on("request", function ($request, $response)
+$server->on("request", function($request, $response)
 {
     $response->header("Server", "Swoole-Phase");
     $response->header("X-Powered-By", "Passion");
