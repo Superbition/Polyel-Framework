1d85eec5accf93ef7c3e43a9b0dc5683a09b281b
embluk <embluk@tuta.io>
2018-11-17 21:32:18 +0100

    Change folder functions to helperFunctions

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . "/core/autoload.php";
-require __DIR__ . "/core/functions/helperFunctions.php";
+require __DIR__ . "/core/helperFunctions/helperFunctions.php";
 
 $server = new Phase_Server();
 
