8e6efdffbf2dc75d022aae1267e766b72c147491
embluk <embluk@tuta.io>
2019-04-21 13:10:08 +0200

    Renamed the framework folder from core to phase

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
-require __DIR__ . "/core/autoload.php";
-require __DIR__ . "/core/helperFunctions/helperFunctions.php";
+require __DIR__ . "/phase/autoload.php";
+require __DIR__ . "/phase/helperFunctions/helperFunctions.php";
 
 $server = new Phase_Server();
 
