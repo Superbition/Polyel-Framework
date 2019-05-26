c8b00381b2e8e5aea37fabb65f267ed329b449c4
embluk <embluk@tuta.io>
2019-05-26 17:44:13 +0200

    Load PHP Traits first during the bootstrap process
    Added some more comments as well.

--- phase/bootstrap.php
+++ phase/bootstrap.php
@@ -4,6 +4,8 @@ echo "Bootstrap process started...\n";
 
 $startingDirectory = new RecursiveDirectoryIterator(__DIR__ . "/src/");
 $pathIterator = new RecursiveIteratorIterator($startingDirectory);
+
+$traits = [];
 $phaseSourceFiles = [];
 
 echo "Building Phase Framework source map\n";
@@ -14,10 +16,22 @@ foreach ($pathIterator as $file)
     // Build up a class/ source map of the Phase Framework. Don't load the autoloader file.
     if(preg_match('/^.+\.php$/i', $currentFile) && !stristr($currentFile, "autoload.php"))
     {
+        // Traits need to be loaded first, so collect them separately
+        if(preg_match("/.trait.php/", strtolower($currentFile)))
+        {
+            // Traits will be merged to be placed at the start of the loading process later
+            $traits[] = $currentFile;
+            continue;
+        }
+
         $phaseSourceFiles[] = $currentFile;
     }
 }
 
+// Put all traits at the start of the source map so they are loaded first.
+$phaseSourceFiles = array_merge($traits, $phaseSourceFiles);
+
+// Loop through each source file and load them in.
 foreach ($phaseSourceFiles as $file)
 {
     // Load each Phase Framework core PHP file to make them available using the class map.
