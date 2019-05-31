14adf1637e7ffabfd88acde75d34dd41471fa7ca
embluk <embluk@tuta.io>
2019-05-31 13:26:10 +0200

    Improved debug.php file explanation

--- debug.example.php
+++ debug.example.php
@@ -4,6 +4,9 @@
  *
  * This file is used for debugging, each Phase project comes with this file and
  * it is only to be used in the development stage. Use this file to test and debug your application as this
- * script gets run upon every HTTP request.
+ * script gets run upon every HTTP request. By default this file is named 'debug.example.php'.
+ *
+ * To make use of this debug file, rename it to 'debug.php' and keep it in the root of your project.
+ * By default it is ignored by Git.
  *
  */
