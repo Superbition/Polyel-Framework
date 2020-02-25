a3709dc42cab6826c9a0a77510e0bd0a8a34651e
embluk <embluk@tuta.io>
2020-02-25 01:49:12 +0000

    Add support to access the Container without injection...
    Uses the main Polyel static class to call the container and pass back
    the requested service/class. The Container is stored within this class.

--- Polyel/src/Polyel.class.php
+++ Polyel/src/Polyel.class.php
@@ -1,11 +1,25 @@
 <?php
 
+use Polyel\Container\Container;
+
 class Polyel
 {
     private static $polyelVersion = "";
 
+    private static $container;
+
     public static function version()
     {
         return self::$polyelVersion;
+    }
+
+    public static function createContainer($baseClass)
+    {
+        self::$container = new Container($baseClass);
+    }
+
+    public static function call($requestedClass)
+    {
+        return self::$container->get($requestedClass);
     }
 }
