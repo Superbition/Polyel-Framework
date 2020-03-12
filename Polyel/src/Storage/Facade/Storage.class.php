bf3ca2479fd7247cbece8109970d2cb6276933a4
embluk <embluk@tuta.io>
2020-03-12 16:11:48 +0000

    Create a static proxy/Facade for the Storage service

---
+++ Polyel/src/Storage/Facade/Storage.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Polyel\Storage\Facade;
+
+use Polyel;
+
+class Storage
+{
+    public static function __callStatic($method, $arguments)
+    {
+        return Polyel::call(Polyel\Storage\Storage::class)->$method($arguments);
+    }
+}
