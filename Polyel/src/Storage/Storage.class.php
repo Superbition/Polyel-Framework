da4cba52300576736dc4f466a9abf777380be93a
embluk <embluk@tuta.io>
2020-03-12 16:11:04 +0000

    Create the main storage class which allows access to sub-storage drivers
    First storage driver being local filesystem access.

---
+++ Polyel/src/Storage/Storage.class.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Polyel\Storage;
+
+class Storage
+{
+    // Holds the storage driver for local operators
+    private $localStorage;
+
+    public function __construct(LocalStorage $localStorage)
+    {
+        $this->localStorage = $localStorage;
+    }
+
+    // The access function is the gateway to all the storage drivers
+    public function access($storageLocation)
+    {
+        // Use a switch to determine which storage driver is needed
+        switch (strtolower(...$storageLocation))
+        {
+            // Local storage driver for local filesystem access
+            case 'local':
+
+                return $this->localStorage;
+
+                break;
+        }
+
+        // Return NULL when no storage driver match is found
+        return NULL;
+    }
+}
