cc47c24db3c7f82befbca919ecca1d21c77f8426
embluk <embluk@tuta.io>
2020-03-19 14:57:37 +0000

    Add a middleware.php config file
    Starting with middleware keys.

---
+++ config/middleware.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+     * Used to shorten Middleware class names for use when
+     * attaching a Middleware to a Route.
+     */
+    "keys" =>
+    [
+        "ExampleMiddleware" => \App\Middleware\ExampleMiddleware::class,
+    ]
+
+];
