b974bbe4d42c83d7ec4355383908ad458f5c89f1
embluk <embluk@tuta.io>
2020-03-20 14:13:08 +0000

    Add a global Middleware config option for before and after...
    Update the Middleware examples to use a class property to define the
    Middleware type of either "before" or "after". This property will then
    be used to set what type a Middleware can be, before or after a request.
    The Middleware stub has also been updated.

--- config/middleware.php
+++ config/middleware.php
@@ -8,7 +8,25 @@ return [
      */
     "keys" =>
     [
-        "ExampleMiddleware" => \App\Middleware\ExampleMiddleware::class,
-    ]
+        "BeforeMiddlewareExample" => \App\Middleware\BeforeExampleMiddleware::class,
+
+        "AfterMiddlewareExample" => \App\Middleware\AfterExampleMiddleware::class,
+    ],
+
+    /*
+     * Middleware which runs globally either before or after a request.
+     * The order of execution is respected from this configuration on both before
+     * and after lists below.
+     */
+    "global" =>
+    [
+        "before" => [
+            //"BeforeMiddlewareExample",
+        ],
+
+        "after" => [
+            //"AfterMiddlewareExample",
+        ],
+    ],
 
 ];
