da6091dc802c1367260b6d51ea1760ffa83f09db
embluk <embluk@tuta.io>
2020-03-20 22:00:08 +0000

    Add a config helper function

--- Polyel/src/helperFunctions/helperFunctions.php
+++ Polyel/src/helperFunctions/helperFunctions.php
@@ -9,3 +9,8 @@ function env($envRequest, $defaultValue)
 {
     return Polyel::call(Polyel\Config\Config::class)->env($envRequest, $defaultValue);
 }
+
+function config($configRequest)
+{
+    return Polyel::call(Polyel\Config\Config::class)->get($configRequest);
+}
