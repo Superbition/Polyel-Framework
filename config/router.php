216a1b4842d6ad0fbce64ab78f899de28a741fad
embluk <embluk@tuta.io>
2020-03-30 14:28:39 +0100

    Better code example

--- config/router.php
+++ config/router.php
@@ -6,7 +6,7 @@ return [
      * Configure the parameter tag used to detect route arguments.
      * This tag is used on route requests, you put your parameter(s) between this tag.
      *
-     * For example, a route of /admin/users/{page}
+     * For example, a route of /blog/post/{id}
      *
      * You can change the tag used to detect these arguments but you must put a space
      * between the delimiter you configure, for example, the default is { }.
