a6ed0ef5b9fe158f6e53dc96f534e57b7bab9905
embluk <embluk@tuta.io>
2020-03-30 14:07:18 +0100

    Created a router config with the first option: routeParameterTag

---
+++ config/router.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+     * Configure the parameter tag used to detect route arguments.
+     * This tag is used on route requests, you put your parameter(s) between this tag.
+     *
+     * For example, a route of /admin/users/{page}
+     *
+     * You can change the tag used to detect these arguments but you must put a space
+     * between the delimiter you configure, for example, the default is { }.
+     */
+    "routeParameterTag" => "{ }",
+
+];
