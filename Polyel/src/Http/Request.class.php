9d0913e0fd95e5b73de75036e808450135515fe8
embluk <embluk@tuta.io>
2020-04-16 21:16:13 +0100

    Added code comments

--- Polyel/src/Http/Request.class.php
+++ Polyel/src/Http/Request.class.php
@@ -69,40 +69,61 @@ class Request
         // If not exists, then return either normal $POST or rawContent()...
         if(exists($inputName))
         {
+            // Check for a single key value pair first
             if(exists($this->postData) && array_key_exists($inputName, $this->postData))
             {
+                // Return a value from a single input name
                 return $this->postData[$inputName];
             }
 
+
+            /*
+             * This if block is used to allow for accessing array values from
+             * normal form POST requests or JSON inputs from rawContent(). Arrays are accessed
+             * via dot syntax and the JSON header must be set to decode JSON objs.
+             * If the input given is using dot syntax, we continue checking for finding an array key/value...
+             */
             $inputArray = explode(".", $inputName);
             if(is_array($inputArray) && exists($inputArray))
             {
+                // Get the normal POST form data as a starting point
                 $postDataArr = $this->postData;
 
+                // Then check for the JSON header, to know when to decode JSON requests
                 if($this->hasHeader("content-type", "application/json"))
                 {
+                    /*
+                     * Decode and convert JSON object to PHP array
+                     * If this block is reached, only JSON data is used for array access...
+                     */
                     $postDataArr = json_decode($this->postRawContent, true);
                 }
 
+                // If POST data is null and no JSON data is found, return false
                 if(!exists($postDataArr))
                 {
+                    // No form data or JSON data was found...
                     return false;
                 }
 
+                // The main loop for finding
                 foreach ($inputArray as $inputItem)
                 {
                     // Loop until we get a final value based on the dot syntax
                     $postDataArr = $postDataArr[$inputItem];
                 }
 
+                // Return data selected from the main loop based on the input name to search with
                 return $postDataArr;
             }
 
+            // No data was found, return the default if it was set
             if(exists($default))
             {
                 return $default;
             }
 
+            // Return false if all else fails, no data found
             return false;
         }
 
@@ -128,21 +149,27 @@ class Request
 
     public function query($queryName = null, $queryDefault = null)
     {
+        // Proceed to find query if a name to search for is set...
         if(exists($queryName))
         {
+            // Check to see if the query name given exists
             if(exists($this->queries[$queryName]))
             {
+                // Return thr query value
                 return $this->queries[$queryName];
             }
 
+            // Return a default if no query was found
             if(exists($queryDefault))
             {
                 return $queryDefault;
             }
 
+            // No query or default set
             return false;
         }
 
+        // No query name set to search for, return the whole query...
         return $this->queries;
     }
 
