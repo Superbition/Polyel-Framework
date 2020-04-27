2f65ee7d98e420802dd7203d47f5705d9b4536fb
embluk <embluk@tuta.io>
2020-04-27 18:01:48 +0100

    Add the JSON pretty print option when encoding JSON data

--- Polyel/src/Http/Utilities/ResponseUtilities.trait.php
+++ Polyel/src/Http/Utilities/ResponseUtilities.trait.php
@@ -6,7 +6,7 @@ trait ResponseUtilities
 {
     private function convertArrayToJson($content)
     {
-        $jsonOptions = JSON_INVALID_UTF8_SUBSTITUTE;
+        $jsonOptions = JSON_INVALID_UTF8_SUBSTITUTE | JSON_PRETTY_PRINT;
         return json_encode($content, $jsonOptions, 1024);
     }
 }
