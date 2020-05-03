a7f4995900bdb3e117037a59c5ac52bd0657ff70
embluk <embluk@tuta.io>
2020-05-03 16:39:43 +0100

    Update comment

--- Polyel/src/services.php
+++ Polyel/src/services.php
@@ -3,6 +3,10 @@
 /*
  * An array to hold all the core services which need to be resolved into the
  * container. The fully qualified namespace is needed to resolve a class.
+ *
+ * This service list is mainly used to auto load classes into the container
+ * that are not directly defined inside a constructor, meaning they won't get
+ * resolved for use.
  */
 return [
 
