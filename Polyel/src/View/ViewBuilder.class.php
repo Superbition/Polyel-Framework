bfd408ea72dc769e66879581afbbf8c082f50baa
embluk <embluk@tuta.io>
2020-05-04 19:04:03 +0100

    Add a ViewBuilder class, used to create a view to return as a response
    Initial class commit.

---
+++ Polyel/src/View/ViewBuilder.class.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Polyel\View;
+
+class ViewBuilder
+{
+    public $resource;
+
+    private $resourceDir = ROOT_DIR . '/app/resources';
+
+    public $validResource = false;
+
+    public $type;
+
+    public $data;
+
+    public function __construct($resource, $data)
+    {
+        $resourceAndType = explode(":", $resource);
+        $this->resource = $resourceAndType[0];
+        $this->type = $resourceAndType[1];
+
+        if($this->type === 'view' && file($this->resourceDir . '/views/' . $this->resource . '.view.html'))
+        {
+            $this->validResource = true;
+        }
+        else if($this->type === 'error' && file($this->resourceDir . '/errors/' . $this->resource . '.error.html'))
+        {
+            $this->validResource = true;
+        }
+
+        if(exists($data) && is_array($data))
+        {
+            $this->data = $data;
+        }
+    }
+}
