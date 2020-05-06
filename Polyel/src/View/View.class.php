77bf5bf982672dcabc41216dfc4f85877a4c1b7c
embluk <embluk@tuta.io>
2020-05-06 15:26:36 +0100

    Add support for key value tag replacement to inject data into views

--- Polyel/src/View/View.class.php
+++ Polyel/src/View/View.class.php
@@ -36,10 +36,54 @@ class View
             $viewLocation = $this->resourceDir . "/${type}s/" . $this->resource . ".${type}.html";
             $this->resource = Storage::access('local')->read($viewLocation);
 
+            if(exists($this->data))
+            {
+                // If data has been passed in, inject that into the resource
+                $this->injectDataToView();
+            }
+
             return $this->resource;
         }
 
         // Return because the resource was not found or is invalid from the ViewBuilder
         return null;
+    }
+
+    private function injectDataToView()
+    {
+        // Get all the tags from the resource template
+        $resourceTags = $this->getResourceTags($this->resource, "{{", "}}");
+
+        foreach($this->data as $key => $value)
+        {
+            if(in_array($key, $resourceTags, true))
+            {
+                $this->resource = str_replace("{{ $key }}", $value, $this->resource);
+            }
+        }
+    }
+
+    private function getResourceTags($resource, $startDelimiter, $endDelimiter): array
+    {
+        $tags = [];
+        $startDelimiterLength = strlen($startDelimiter);
+        $endDelimiterLength = strlen($endDelimiter);
+        $startFrom = $resourceStart = $resourceEnd = 0;
+
+        while (false !== ($resourceStart = strpos($resource, $startDelimiter, $startFrom)))
+        {
+            $resourceStart += $startDelimiterLength;
+            $resourceEnd = strpos($resource, $endDelimiter, $resourceStart);
+
+            if (false === $resourceEnd)
+            {
+                break;
+            }
+
+            $tags[] = trim(substr($resource, $resourceStart, $resourceEnd - $resourceStart));
+            $startFrom = $resourceEnd + $endDelimiterLength;
+        }
+
+        return $tags;
     }
 }
