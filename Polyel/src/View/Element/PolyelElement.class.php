203a7e5465be21d87c8becca96245ff8e1118818
embluk <embluk@tuta.io>
2020-05-09 17:16:17 +0100

    Added Base Element functions setData() and appendLine()

--- Polyel/src/View/Element/PolyelElement.class.php
+++ Polyel/src/View/Element/PolyelElement.class.php
@@ -7,8 +7,37 @@ class PolyelElement
     // Holds the final rendered element content
     private $element = '';
 
+    private $data;
+
     public function __construct()
     {
 
+    }
+
+    protected function setData($tag, $data = null)
+    {
+        if(is_array($tag))
+        {
+            foreach($tag as $key => $value)
+            {
+                $this->data[$key] = $value;
+            }
+        }
+        else if(exists($tag) && exists($data))
+        {
+            $this->data[$tag] = $data;
+        }
+    }
+
+    protected function appendLine($start, $data, $end, $xssFilter = true)
+    {
+        if($xssFilter)
+        {
+            $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+        }
+
+        $append = $start . $data . $end;
+
+        $this->element .= $append;
     }
 }
