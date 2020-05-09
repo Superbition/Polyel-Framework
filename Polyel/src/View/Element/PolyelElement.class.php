61096b39b72f75fd7a085caef713327262d84388
embluk <embluk@tuta.io>
2020-05-09 15:41:40 +0100

    Created example View\Element class and starting point for a Base Element
    Base element called PolyelElement.

---
+++ Polyel/src/View/Element/PolyelElement.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Polyel\View\Element;
+
+class PolyelElement
+{
+    // Holds the final rendered element content
+    private $element;
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ app/View/Elements/Element.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\View\Elements;
+
+use Polyel\View\Element\PolyelElement;
+
+class Element extends PolyelElement
+{
+    // Set the element template relative to /app/resources/elements/
+    public $elementTemplate = 'element';
+
+    public function __construct()
+    {
+
+    }
+
+    public function render()
+    {
+
+    }
+}
