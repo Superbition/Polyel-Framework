6b15b796d3d6da7458adf8eefe79ca389ead8b85
embluk <embluk@tuta.io>
2020-05-11 15:25:06 +0100

    New common function replaceTag() in ViewTools

--- Polyel/src/View/ViewTools.trait.php
+++ Polyel/src/View/ViewTools.trait.php
@@ -27,4 +27,24 @@ trait ViewTools
 
         return $matches;
     }
+
+    private function replaceTag($key, $value, &$tags, &$content)
+    {
+        // Check if the key we want to replace exists inside the collected tags from the content
+        if(in_array($key, $tags, true))
+        {
+            // Automatically filter data tags for XSS prevention
+            $xssEscapedData = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+            $content = str_replace("{{ $key }}", $xssEscapedData, $content);
+        }
+        else
+        {
+            // Else check to see if the key is matched wanting no XSS filtering
+            if(in_array("!$key!", $tags, true))
+            {
+                // Else raw input has been requested by using {{ !data! }}
+                $content = str_replace("{{ !$key! }}", $value, $content);
+            }
+        }
+    }
 }
