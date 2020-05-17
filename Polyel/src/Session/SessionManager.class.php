461e52956a11a7b8315962bfef819a45acabe966
embluk <embluk@tuta.io>
2020-05-17 14:00:04 +0100

    Create a SessionManager: Starts a new session with cookie and blank file
    Local session storage only for now, MVP driver is local disk.

---
+++ Polyel/src/Session/SessionManager.class.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Polyel\Session;
+
+use Polyel\Http\Facade\Cookie;
+use Polyel\Storage\Facade\Storage;
+
+class SessionManager
+{
+    private $sessionFileStorage = ROOT_DIR . '/storage/polyel/sessions/';
+
+    public function __construct()
+    {
+
+    }
+
+    public function startSession($sessionCookie)
+    {
+        if(!exists($sessionCookie))
+        {
+            $prefix = config('session.prefix');
+
+            do {
+
+                $sessionID = $this->generateSessionID($prefix, 42);
+
+            } while($this->collisionCheckID($sessionID) === true);
+
+            $this->createNewSession($sessionID);
+            $this->queueSessionCookie($sessionID);
+        }
+    }
+
+    private function generateSessionID($prefix, $length): string
+    {
+        $sessionPrefix = $prefix;
+        $sessionID = $sessionPrefix;
+        $idStringSpace = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        $max = strlen($idStringSpace);
+
+        for ($i=0; $i < $length; $i++)
+        {
+            $sessionID .= $idStringSpace[random_int(0, $max-1)];
+        }
+
+        return $sessionID;
+    }
+
+    private function collisionCheckID($sessionID)
+    {
+        if(file_exists($this->sessionFileStorage . $sessionID))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function createNewSession($sessionID)
+    {
+        $sessionFilePath = '/storage/polyel/sessions/' . $sessionID;
+        Storage::access('local')->write($sessionFilePath, '');
+    }
+
+    private function queueSessionCookie($sessionID)
+    {
+        $sessionLifetime = config('session.lifetime');
+
+        if($sessionLifetime !== 0 && is_numeric($sessionLifetime))
+        {
+            $sessionLifetime *= 60;
+        }
+
+        $sessionCookie = [
+            $name = config('session.cookieName'),
+            $value = $sessionID,
+            $expire = $sessionLifetime,
+            $path = config('session.cookiePath'),
+            $domain = config('session.domain'),
+            $secure = config('session.secure'),
+            $httpOnly = config('session.httpOnly'),
+            $sameSite = 'None',
+        ];
+
+        Cookie::queue(...$sessionCookie);
+    }
+}
