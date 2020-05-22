df5773bfbf54614d28805bb38018d33aafdfd861
embluk <embluk@tuta.io>
2020-05-22 23:12:10 +0100

    When a session is regenerated, update the current session ID property

--- Polyel/src/Session/SessionManager.class.php
+++ Polyel/src/Session/SessionManager.class.php
@@ -44,7 +44,7 @@ class SessionManager
         // Either cookie does not exist or the session is missing on the server
         if(!exists($sessionCookieID) || $this->driver->isValid($sessionCookieID, $sessionData) === false)
         {
-            $this->currentRequestSessionID = $this->regenerateSession();
+            $this->regenerateSession();
         }
     }
 
@@ -60,6 +60,8 @@ class SessionManager
 
         $this->driver->createNewSession($sessionID, $this->request);
         $this->queueSessionCookie($sessionID);
+
+        $this->currentRequestSessionID = $sessionID;
 
         return $sessionID;
     }
