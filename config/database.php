b73c7ccb15c4a97f60eb9f1f2598e5d4ba3e1180
embluk <embluk@tuta.io>
2020-05-25 14:53:31 +0100

    Rework database connections config file structure

--- config/database.php
+++ config/database.php
@@ -2,33 +2,34 @@
 
 return [
 
-    "Main_Database" => [
+    "connections" => [
 
-        "driver" => "mysql",
+        "mysql" => [
 
-        "database" => env("Main_Database.DATABASE", "polyel1"),
+            "MainDatabase" => [
 
-        "host" => env("Main_Database.HOST", "127.0.0.1"),
+                "driver" => "mysql",
+                "database" => env("Main_Database.DATABASE", "polyel1"),
+                "host" => env("Main_Database.HOST", "127.0.0.1"),
+                "port" => env("Main_Database.PORT", "3306"),
+                "username" => env("Main_Database.USERNAME", "polyel"),
+                "password" => env("Main_Database.PASSWORD", ""),
 
-        "port" => env("Main_Database.PORT", "3306"),
+            ],
 
-        "username" => env("Main_Database.USERNAME", "polyel"),
+            "SecondDatabase" => [
 
-        "password" => env("Main_Database.PASSWORD", ""),
+                "driver" => "mysql",
+                "database" => env("Second_Database.DATABASE", "polyel2"),
+                "host" => env("Second_Database.HOST", "127.0.0.1"),
+                "port" => env("Second_Database.PORT", "3306"),
+                "username" => env("Second_Database.USERNAME", "polyel"),
+                "password" => env("Second_Database.PASSWORD", ""),
+
+            ],
+
+        ],
+
     ],
 
-    "Second_Database" => [
-
-        "driver" => "mysql",
-
-        "database" => env("Second_Database.DATABASE", "polyel2"),
-
-        "host" => env("Second_Database.HOST", "127.0.0.1"),
-
-        "port" => env("Second_Database.PORT", "3306"),
-
-        "username" => env("Second_Database.USERNAME", "polyel"),
-
-        "password" => env("Second_Database.PASSWORD", ""),
-    ],
 ];
