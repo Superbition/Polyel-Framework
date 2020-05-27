9e8d04c6d974810dee34cad2a5f4f3af87771a2b
embluk <embluk@tuta.io>
2020-05-27 13:39:36 +0100

    Moved database config connections into connections.mysql.databases

--- config/database.php
+++ config/database.php
@@ -8,31 +8,35 @@ return [
 
         "mysql" => [
 
-            "DefaultDatabase" => [
+            "databases" => [
 
-                "driver" => "mysql",
-                "database" => env("Main_Database.DATABASE", "polyel1"),
-                "host" => env("Main_Database.HOST", "127.0.0.1"),
-                "port" => env("Main_Database.PORT", "3306"),
-                "username" => env("Main_Database.USERNAME", "polyel"),
-                "password" => env("Main_Database.PASSWORD", ""),
-                "charset" => "utf8mb4",
-                "collation" => "utf8mb4_unicode_ci",
-                "prefix" => "",
+                "DefaultDatabase" => [
 
-            ],
+                    "driver" => "mysql",
+                    "database" => env("Main_Database.DATABASE", "polyel1"),
+                    "host" => env("Main_Database.HOST", "127.0.0.1"),
+                    "port" => env("Main_Database.PORT", "3306"),
+                    "username" => env("Main_Database.USERNAME", "polyel"),
+                    "password" => env("Main_Database.PASSWORD", ""),
+                    "charset" => "utf8mb4",
+                    "collation" => "utf8mb4_unicode_ci",
+                    "prefix" => "",
 
-            "SecondDatabase" => [
+                ],
 
-                "driver" => "mysql",
-                "database" => env("Second_Database.DATABASE", "polyel2"),
-                "host" => env("Second_Database.HOST", "127.0.0.1"),
-                "port" => env("Second_Database.PORT", "3306"),
-                "username" => env("Second_Database.USERNAME", "polyel"),
-                "password" => env("Second_Database.PASSWORD", ""),
-                "charset" => "utf8mb4",
-                "collation" => "utf8mb4_unicode_ci",
-                "prefix" => "",
+                "SecondDatabase" => [
+
+                    "driver" => "mysql",
+                    "database" => env("Second_Database.DATABASE", "polyel2"),
+                    "host" => env("Second_Database.HOST", "127.0.0.1"),
+                    "port" => env("Second_Database.PORT", "3306"),
+                    "username" => env("Second_Database.USERNAME", "polyel"),
+                    "password" => env("Second_Database.PASSWORD", ""),
+                    "charset" => "utf8mb4",
+                    "collation" => "utf8mb4_unicode_ci",
+                    "prefix" => "",
+
+                ],
 
             ],
 
