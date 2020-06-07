ea8257065a07afdf04f7060058e2e7e46fa9884f
embluk <embluk@tuta.io>
2020-06-07 16:37:25 +0100

    Spacing

--- Polyel/src/Database/Connection/DatabaseConnection.class.php
+++ Polyel/src/Database/Connection/DatabaseConnection.class.php
@@ -43,7 +43,7 @@ class DatabaseConnection
 
     public function lastActive()
     {
-        return$this->lastActive;
+        return $this->lastActive;
     }
 
     public function transaction($status)
