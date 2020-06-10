3676213c46222c83bc23c9dff1bd4fd2bdab2249
embluk <embluk@tuta.io>
2020-06-10 16:36:01 +0100

    Add support for chunking results by ID

--- Polyel/src/Database/Support/Chunk.trait.php
+++ Polyel/src/Database/Support/Chunk.trait.php
@@ -55,4 +55,57 @@ trait Chunk
             });
         });
     }
+
+    public function chunkById($count, Closure $callback, $column = null)
+    {
+        // Default column for chunk by id is 'id'
+        $column = $column ?? 'id';
+
+        // At first we don't have or know the last id value
+        $lastId = null;
+
+        do
+        {
+            // Clone to avoid duplicate queries and query data for every chunk round
+            $clone = clone $this;
+
+            // Use the id column if it exists, first chunk will grab the last id...
+            if(exists($lastId))
+            {
+                // Once we have the last id from the first chunk, we continue onwards to chunk by id
+                $clone->where($column, '>', $lastId);
+            }
+
+            // Order by the id column and limit by the chunk count, this also gets us the first id in order
+            $clone->orderBy($column, 'ASC')->limit($count);
+
+            $results = $clone->get();
+
+            // Get the chunk result set count
+            $chunkCount = count($results);
+
+            // Break out of the loop if there are no results to process
+            if($chunkCount === 0)
+            {
+                break;
+            }
+
+            // Keep calling the anonymous function until chunking completes or false is returned
+            if($callback($results) === false)
+            {
+                // Means we should stop chunking on a false return
+                return false;
+            }
+
+            // Grab the last id from the chunk results, so we can continue to chunk by id
+            $lastId = end($results)[$column];
+
+            // Free up memory
+            unset($results);
+
+        }while($chunkCount === $count);
+
+        // Chunking complete
+        return true;
+    }
 }
