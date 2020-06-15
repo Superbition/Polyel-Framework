858119763ab05b6c85a17ece7b0eb5d3b8040291
embluk <embluk@tuta.io>
2020-06-15 21:15:00 +0100

    Add a Http Kernel to be the centre of a request/response cycle

---
+++ Polyel/src/Http/Kernel.class.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Polyel\Http;
+
+use Polyel\Container\Container;
+
+class Kernel
+{
+    // The kernel service container for this HTTP request
+    public $container;
+
+    // The request service for the duration of this HTTP request
+    public $request;
+
+    // The response service for the duration of this HTTP request
+    public $response;
+
+    public function __construct()
+    {
+        $this->container = new Container();
+        $this->request = $this->container->resolveClass(Request::class);
+        $this->response = $this->container->resolveClass(Response::class);
+    }
+}
