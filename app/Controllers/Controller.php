049d5a34ecfeebe95ca45aa266d5eedba59d954f
embluk <embluk@tuta.io>
2020-06-17 21:03:25 +0100

    Add empty constructors to the welcome & base controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -6,5 +6,8 @@ use Polyel\Controller\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    public function __construct()
+    {
 
+    }
 }
--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -4,6 +4,11 @@ namespace App\Controllers;
 
 class WelcomeController extends Controller
 {
+    public function __construct()
+    {
+
+    }
+
     public function welcome()
     {
         return response(view('welcome:view'));
