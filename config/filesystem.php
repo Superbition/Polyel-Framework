39fe6bd70bee0fd258010e50235768f717f5db05
embluk <embluk@tuta.io>
2020-06-18 18:24:11 +0100

    Add detailed code comments to the filesystem config

--- config/filesystem.php
+++ config/filesystem.php
@@ -2,8 +2,26 @@
 
 return [
 
+    /*
+    │------------------------------------------------------------------------------
+    │ Default Storage Drive
+    │------------------------------------------------------------------------------
+    │
+    │
+    */
     "default" => "local",
 
+    /*
+    │------------------------------------------------------------------------------
+    │ Configured Storage Drivers
+    │------------------------------------------------------------------------------
+    │ You can configure all your drivers for your application. Each drive needs
+    | a driver so Polyel knows how to access the drive you want to interact with.
+    | You may add as many drives as you need, each drive must have a unique name.
+    |
+    | Supported storage drivers: "local" (More coming)
+    │
+    */
     "drives" => [
 
         "local" => [
