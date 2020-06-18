a20bf2112eceaee331c3b3273d765aff75386d76
embluk <embluk@tuta.io>
2020-06-18 17:46:03 +0100

    Add all available storage functions to the Storage Facade using DocBlock

--- Polyel/src/Storage/Facade/Storage.class.php
+++ Polyel/src/Storage/Facade/Storage.class.php
@@ -4,6 +4,20 @@ namespace Polyel\Storage\Facade;
 
 use Polyel;
 
+/**
+ * @method static drive($drive = null)
+ * @method static access($driver, $root)
+ * @method static write($filePath, $contents = "")
+ * @method static size($filePath)
+ * @method static read($filePath)
+ * @method static prepend($filePath, $contents)
+ * @method static append($filePath, $contents)
+ * @method static copy($source, $dest)
+ * @method static move($oldName, $newName)
+ * @method static delete($filePath)
+ * @method static makeDir($dirPath, $mode = 0777)
+ * @method static removeDir($dirPath)
+ */
 class Storage
 {
     public static function __callStatic($method, $arguments)
