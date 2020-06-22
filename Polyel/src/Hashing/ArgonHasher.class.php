a0c2e75c3a06de152320b4595071202b998ff19e
embluk <embluk@tuta.io>
2020-06-22 14:53:44 +0100

    Add support for argon2i and argon2id

---
+++ Polyel/src/Hashing/ArgonHasher.class.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Polyel\Hashing;
+
+use RuntimeException;
+
+class ArgonHasher extends BaseHasher Implements Hasher
+{
+    protected $argonAlgo;
+
+    protected $memory;
+
+    protected $time;
+
+    protected $threads;
+
+    public function __construct(array $options)
+    {
+        $this->memory = $options['memory'] ?? 1024;
+        $this->time = $options['time'] ?? 2;
+        $this->threads = $options['threads'] ?? 2;
+    }
+
+    public function setArgonType($argonAlgo)
+    {
+        $this->argonAlgo = $argonAlgo;
+    }
+
+    public function make($value, array $options = [])
+    {
+        $memory = $options['memory'] ?? $this->memory;
+        $time = $options['time'] ?? $this->time;
+        $threads = $options['threads'] ?? $this->threads;
+
+        $hash = password_hash($value, $this->argonAlgo, [
+            'memory_cost' => $memory,
+            'time_cost' => $time,
+            'threads' => $threads,
+        ]);
+
+        if($hash === false)
+        {
+            throw new RuntimeException('Argon2 algorithms are not available for hashing');
+        }
+
+        return $hash;
+    }
+
+    public function check($value, $hashedValue)
+    {
+        if($this->info($hashedValue)['algo'] !== $this->argonAlgo)
+        {
+            throw new RuntimeException('The given hash does not use the Argon2 algorithms');
+        }
+
+        return Parent::check($value, $hashedValue);
+    }
+
+    public function needsRehash($hashedValue, array $options = [])
+    {
+        $memory = $options['memory'] ?? $this->memory;
+        $time = $options['time'] ?? $this->time;
+        $threads = $options['threads'] ?? $this->threads;
+
+        return password_needs_rehash($hashedValue, $this->argonAlgo, [
+            'memory_cost' => $memory,
+            'time_cost' => $time,
+            'threads' => $threads,
+        ]);
+    }
+}
--- Polyel/src/Hashing/HashManager.class.php
+++ Polyel/src/Hashing/HashManager.class.php
@@ -6,6 +6,11 @@ class HashManager implements Hasher
 {
     private $hasher;
 
+    private $argonAlgos = [
+        'argon2i' => PASSWORD_ARGON2I,
+        'argon2id' => PASSWORD_ARGON2ID
+    ];
+
     public function __construct()
     {
 
@@ -20,6 +25,14 @@ class HashManager implements Hasher
             case 'bcrypt':
 
                 $this->hasher = new BcryptHasher(config("hashing.$algoName"));
+
+            break;
+
+            case 'argon2i':
+            case 'argon2id':
+
+                $this->hasher = new ArgonHasher(config("hashing.argon"));
+                $this->hasher->setArgonType($this->argonAlgos[$algoName]);
 
             break;
         }
