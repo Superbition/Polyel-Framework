6166e34a8feec03ff626a5e9d79c8cd86cd300a0
embluk <embluk@tuta.io>
2020-06-24 09:03:02 +0100

    Add description comment for API routes file

--- app/routing/api.php
+++ app/routing/api.php
@@ -6,7 +6,11 @@ use Polyel\Router\Facade\Route;
 │------------------------------------------------------------------------------
 │ API Routes
 │------------------------------------------------------------------------------
-│
+│ This is the API routes for your application, here you can register routes
+| that are a part of your API, and these routes are then loaded during server
+| boot after registering web routes.
+| You may use the group method to assign a prefix or a set of middleware
+| to multiple API routes at a time. An example has been defined below...
 │
 */
 
