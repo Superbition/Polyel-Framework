d2cfe60dc663336016279f15618694ac855620b9
embluk <embluk@tuta.io>
2020-06-27 20:35:32 +0100

    Map out the initial class for the database auth driver

---
+++ Polyel/src/Auth/SourceDrivers/Database.class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Polyel\Auth\SourceDrivers;
+
+use Polyel\Database\Facade\DB;
+
+class Database
+{
+    // The table containing the users
+    private string $table;
+
+    public function __construct()
+    {
+
+    }
+
+    public function getUserById($id)
+    {
+
+    }
+
+    public function getUserByToken($token)
+    {
+
+    }
+
+    public function getUserByCredentials($credentials)
+    {
+
+    }
+}
