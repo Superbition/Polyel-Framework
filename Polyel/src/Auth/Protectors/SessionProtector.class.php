4d259c055c01bbcfad044d442ecca2bae91c2586
embluk <embluk@tuta.io>
2020-06-28 15:11:42 +0100

    Implement check() and user load() for the SessionProtector...
    Checking when a user is authed & performing the first time user loading
    during a request with load()

--- Polyel/src/Auth/Protectors/SessionProtector.class.php
+++ Polyel/src/Auth/Protectors/SessionProtector.class.php
@@ -2,12 +2,17 @@
 
 namespace Polyel\Auth\Protectors;
 
+use Polyel\Http\Request;
 use Polyel\Session\Session;
+use Polyel\Auth\AuthenticatedUser;
 use Polyel\Auth\SourceDrivers\Database;
 
 class SessionProtector
 {
     private $session;
+
+    // Where the authenticated user is stored during a request
+    private $user;
 
     private $users;
 
@@ -15,5 +20,47 @@ class SessionProtector
     {
         $this->session = $session;
         $this->users = $users;
+    }
+
+    public function check(Request $request)
+    {
+        // Get the user ID from the session system
+        $user = $this->session->user();
+
+        // Make sure the user ID exists in the session
+        if(!is_null($user))
+        {
+            // Load the user if not already loaded for this request...
+            if(!exists($this->user))
+            {
+                // Called when the user has not yet been loaded but has a ID in the session
+                $this->user = $this->load($user);
+            }
+
+            // If an AuthenticatedUser exists, it means we have a valid authenticated user
+            if($this->user instanceof AuthenticatedUser)
+            {
+                return true;
+            }
+        }
+
+        // Invalid, the user is not properly authenticated...
+        return false;
+    }
+
+    private function load($user)
+    {
+        // Try and find the user by the ID from the session...
+        $user = $this->users->getUserById($user);
+
+        // If we find the user, create a new AuthenticatedUser instance
+        if(exists($user))
+        {
+            // Return a newly authenticated user and their data...
+            return new AuthenticatedUser($user);
+        }
+
+        // The user could not be found by their ID, invalid user, not authenticated...
+        return false;
     }
 }
