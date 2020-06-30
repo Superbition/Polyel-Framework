c0bdc053f89758c114d4e9b9c5d45c6f77334fc6
embluk <embluk@tuta.io>
2020-06-30 12:04:53 +0100

    Add a username property for the LoginController...
    Indicates the field to be used when trying to find a
    user in the database using data from a POST request.

--- app/Controllers/Auth/LoginController.php
+++ app/Controllers/Auth/LoginController.php
@@ -10,6 +10,15 @@ class LoginController extends Controller
 {
     use AuthLogin;
 
+    /*
+     * The field to be used as the main username when trying to
+     * login a user from the request data obtained from the login
+     * POST request. By default this is set to be 'email' but you may
+     * change this to something else, just make sure it links to your
+     * form names.
+     */
+    private string $username = 'email';
+
     private $auth;
 
     public function __construct(AuthManager $auth)
