e936c15748c30ba38731d1186a970c4e1a3c5396
embluk <embluk@tuta.io>
2020-07-01 12:03:13 +0100

    Implement the authentication Middleware to check for valid authed users

--- Polyel/src/Auth/Middleware/Authenticate.class.php
+++ Polyel/src/Auth/Middleware/Authenticate.class.php
@@ -17,6 +17,36 @@ abstract class Authenticate implements AuthenticationOutcomes
 
     public function process($request, $protector = null)
     {
+        // Get the default protector is one is not provided...
+        $protector = $protector ?: $this->getDefaultProtector();
 
+        // Using the selected protector, set the source table to be used when authenticating users
+        $this->auth->setSource($protector['source']);
+
+        // Try and authenticate the user and get the outcome...
+        $authenticated = $this->authenticate($request, $protector);
+
+        // If false, it means the user is not authenticated...
+        if($authenticated === false)
+        {
+            // Return an unauthenticated response
+            return $this->unauthenticated();
+        }
+
+        // Return an authenticated response
+        return $this->authenticated();
+    }
+
+    private function authenticate($request, $protector)
+    {
+        // Check the request to see if it is authenticated...
+        return $this->auth->protector($protector['driver'])->check($request);
+    }
+
+    private function getDefaultProtector()
+    {
+        // Grabs the default protector that is set in the auth.php config file
+        $defaultProtector = config('auth.defaults.protector');
+        return config("auth.protectors.$defaultProtector");
     }
 }
