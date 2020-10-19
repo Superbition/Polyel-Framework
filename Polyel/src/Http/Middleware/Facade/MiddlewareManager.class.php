4c75c56fe6ea3285c26c9f62d5a0e898e7b808b4
embluk <embluk@tuta.io>
2020-10-19 22:50:19 +0100

    Add PHP DocBlock for public MiddlewareManager Facade methods

--- Polyel/src/Http/Middleware/Facade/MiddlewareManager.class.php
+++ Polyel/src/Http/Middleware/Facade/MiddlewareManager.class.php
@@ -4,6 +4,15 @@ namespace Polyel\Http\Middleware\Facade;
 
 use Polyel;
 
+/**
+ * Class MiddlewareManager
+ *
+ * @method static generateStackForRoute($requestMethod, $requestUrl)
+ *
+ * @method static prepareStack($HttpKernel, $routeMiddlewareStack, $routeMiddlewareAliases, $globalMiddlewareStack)
+ *
+ * @method static executeStackWithCoreAction($HttpKernel, $middlewareStack, $coreAction)
+ */
 class MiddlewareManager
 {
     public static function __callStatic($method, $arguments)
