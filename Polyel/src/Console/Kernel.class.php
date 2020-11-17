758047079aec0d6c981d88d21082aff475297623
embluk <embluk@tuta.io>
2020-11-17 15:35:03 +0000

    Register internal core console commands and merge internal aliases
    Include the alias for the list command as a starting point.

--- Polyel/src/Console/Kernel.class.php
+++ Polyel/src/Console/Kernel.class.php
@@ -8,14 +8,28 @@ class Kernel
 
     protected $defaultCommand = 'list';
 
+    protected array $commandAliases = [];
+
     public function __construct(ConsoleApplication $console)
     {
         $this->console = $console;
         $this->console->loadCommandsFrom('/routing/console.php');
+        $this->console->loadCommandsFrom('/Polyel/src/Console/Commands/console.php');
+
+        $this->registerInternalCommandAliases();
     }
 
     public function process(Input $input)
     {
         $input->parseCommandInput();
+    }
+
+    private function registerInternalCommandAliases()
+    {
+        $this->commandAliases = array_merge($this->commandAliases, [
+
+            'list' => Commands\ListCommand::class,
+
+        ]);
     }
 }
