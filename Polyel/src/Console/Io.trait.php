e942e889b6ddf98afe7e166dcaf3e4387e9bb779
embluk <embluk@tuta.io>
2020-11-22 20:10:22 +0000

    Implement input methods ask() and askSecret() for console input
    Allows getting input from the console and hiding input for
    sensitive data like passwords with askSecret().

--- Polyel/src/Console/Io.trait.php
+++ Polyel/src/Console/Io.trait.php
@@ -6,6 +6,9 @@ trait Io
 {
     private $defaultStyle = "\e[0m";
 
+    // By default stty availability isn't checked, so we set it to false to start with
+    private $sttyAvailable = false;
+
     public function writeLine(string $output)
     {
         if($this->verbosityLevel !== -1)
@@ -63,5 +66,65 @@ trait Io
     {
         fwrite(STDERR, "\e[41;1;33m[Fatal]$this->defaultStyle $fatal\n");
         exit("Exiting early because of fatal error!\n\n");
+    }
+
+    public function ask($question)
+    {
+        $answer = readline($question);
+
+        readline_add_history($question);
+
+        return $answer;
+    }
+
+    public function askSecret($question)
+    {
+        // Output the question prompt
+        $this->writeLine($question);
+
+        // The default availability status for the stty command is false, so we can check if it is available...
+        if($this->sttyAvailable === false)
+        {
+            // Run a shell command to check if the stty command is available or not
+            $sttyAvailable = \Swoole\Coroutine\System::exec('stty 2>&1');
+
+            // If the exit code is 0, it means successful and that we have the stty available to us
+            if($sttyAvailable['code'] === 0)
+            {
+                // So that we don't have to check availability again
+                $this->sttyAvailable = true;
+            }
+            else
+            {
+                /*
+                 * Else the exit code was more than 0, meaning the stty command is not available
+                 *
+                 * This fatal call will also force an exit return
+                 */
+                $this->fatal("stty command not available: Unable hide response.");
+            }
+        }
+
+        /*
+         * The stty command allows us to turn off echoing so that
+         * the console input for a secret does not show up when
+         * the user types their response but, we first get the original
+         * terminal mode, so we can later reset the terminal to its
+         * original settings before we turned echoing off to hide
+         * input.
+         */
+        $terminalMode = \Swoole\Coroutine\System::exec('stty -g')['output'];
+        \Swoole\Coroutine\System::exec('stty -echo');
+
+        // Capture the secret input
+        $secret = fgets(STDIN);
+
+        // Re-enable the terminal with its original settings and echoing back on
+        \Swoole\Coroutine\System::exec("stty $terminalMode");
+
+        // Output a newline char
+        $this->writeNewLine(' ');
+
+        return trim($secret);
     }
 }
