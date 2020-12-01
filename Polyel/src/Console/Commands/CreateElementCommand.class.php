466b4c654d0ab03f740ce42bdd83c73053b997c5
embluk <embluk@tuta.io>
2020-12-01 01:11:10 +0000

    Add an option to create the element template as well with create:element
    New option for create:element called: --element-template

--- Polyel/src/Console/Commands/CreateElementCommand.class.php
+++ Polyel/src/Console/Commands/CreateElementCommand.class.php
@@ -11,6 +11,7 @@ class CreateElementCommand extends Command
     public function execute()
     {
         $elementName = $this->argument('element-name');
+        $elementTemplate = $this->option('--element-template');
 
         $this->writeNewLine('Building View Element stub source and destination file paths');
         $sourceStub = ROOT_DIR . '/Polyel/src/Console/stubs/Element.stub';
@@ -19,9 +20,26 @@ class CreateElementCommand extends Command
         $this->writeNewLine('Generating a new Element class...');
         copy($sourceStub, $distElement);
 
-        $this->writeNewLine('Replacing Element placeholders', 2);
+        $this->writeNewLine('Replacing Element placeholders');
         $newElementClass = str_replace('{{ ElementClassName }}', trim($elementName), file_get_contents($distElement));
+
+        if($elementTemplate !== false)
+        {
+            $this->info('Template element name given, creating template file as well');
+            file_put_contents(ROOT_DIR . "/resources/elements/$elementTemplate.html", '');
+
+            $newElementClass = str_replace('{{ ElementTemplate }}', "'$elementTemplate'", $newElementClass);
+        }
+        else
+        {
+            $this->info('No element template file name given, only creating element class');
+
+            $newElementClass = str_replace('{{ ElementTemplate }}', "''", $newElementClass);
+        }
+
         file_put_contents($distElement, $newElementClass);
+
+        $this->writeNewLine('');
 
         $this->writeNewLine("\e[32mCreated a new View Element called: $elementName", 2);
     }
