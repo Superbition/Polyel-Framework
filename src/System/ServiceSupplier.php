3c9202389819dd97c96a46802045920b3f72f339
embluk <embluk@tuta.io>
2021-01-07 23:58:25 +0000

    Add methods that can be used to register service binds and singletons
    Includes: registerBind, registerRequestSingleton and
    registerServerSingleton. Keeps track of registered services in arrays.

--- src/System/ServiceSupplier.php
+++ src/System/ServiceSupplier.php
@@ -2,12 +2,35 @@
 
 namespace Polyel\System;
 
+use Closure;
+
 abstract class ServiceSupplier
 {
+    private array $binds = [];
+
+    private array $requestSingletons = [];
+
+    private array $serverSingletons = [];
+
     public function __construct()
     {
 
     }
 
     abstract public function register();
+
+    protected function registerBind(string $classToBind, Closure $classServiceSupplier)
+    {
+        $this->binds[] = ['class' => $classToBind, 'supplier' => $classServiceSupplier];
+    }
+
+    protected function registerRequestSingleton(string $requestSingletonClass, Closure $requestSingletonSupplier)
+    {
+        $this->requestSingletons[] = ['class' => $requestSingletonClass, 'supplier' => $requestSingletonSupplier];
+    }
+
+    protected function registerServerSingleton(string $serverSingletonClass, Closure $serverSingletonSupplier)
+    {
+        $this->serverSingletons[] = ['class' => $serverSingletonClass, 'supplier' => $serverSingletonSupplier];
+    }
 }
