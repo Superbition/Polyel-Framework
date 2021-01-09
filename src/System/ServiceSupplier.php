b6f84e0005e19dbc1f147488edeb6f49b2921ad7
embluk <embluk@tuta.io>
2021-01-09 17:29:28 +0000

    Change bind & singletons anonymous function index name to 'closure'

--- src/System/ServiceSupplier.php
+++ src/System/ServiceSupplier.php
@@ -21,16 +21,16 @@ abstract class ServiceSupplier
 
     protected function registerBind(string $classToBind, Closure $classServiceSupplier)
     {
-        $this->binds[] = ['class' => $classToBind, 'supplier' => $classServiceSupplier];
+        $this->binds[] = ['class' => $classToBind, 'closure' => $classServiceSupplier];
     }
 
     protected function registerRequestSingleton(string $requestSingletonClass, Closure $requestSingletonSupplier)
     {
-        $this->requestSingletons[] = ['class' => $requestSingletonClass, 'supplier' => $requestSingletonSupplier];
+        $this->requestSingletons[] = ['class' => $requestSingletonClass, 'closure' => $requestSingletonSupplier];
     }
 
     protected function registerServerSingleton(string $serverSingletonClass, Closure $serverSingletonSupplier)
     {
-        $this->serverSingletons[] = ['class' => $serverSingletonClass, 'supplier' => $serverSingletonSupplier];
+        $this->serverSingletons[] = ['class' => $serverSingletonClass, 'closure' => $serverSingletonSupplier];
     }
 }
