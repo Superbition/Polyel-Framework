c2ec02a9b31998f345e8f65c1e7823626fe214a3
embluk <embluk@tuta.io>
2021-01-10 00:53:36 +0000

    Add the ability to mark singleton services to be container deferred

--- src/System/ServiceSupplier.php
+++ src/System/ServiceSupplier.php
@@ -12,12 +12,29 @@ abstract class ServiceSupplier
 
     private array $serverSingletons = [];
 
+    private string $lastAddedSingletonType;
+
     public function __construct()
     {
 
     }
 
     abstract public function register();
+
+    public function defer()
+    {
+        if(!is_null($this->lastAddedSingletonType))
+        {
+            if($this->lastAddedSingletonType === 'server')
+            {
+                $this->serverSingletons[array_key_last($this->serverSingletons)]['defer'] = true;
+            }
+            else if($this->lastAddedSingletonType === 'request')
+            {
+                $this->requestSingletons[array_key_last($this->requestSingletons)]['defer'] = true;
+            }
+        }
+    }
 
     protected function registerBind(string $classToBind, Closure $classServiceSupplier)
     {
@@ -26,11 +43,27 @@ abstract class ServiceSupplier
 
     protected function registerRequestSingleton(string $requestSingletonClass, Closure $requestSingletonSupplier)
     {
-        $this->requestSingletons[] = ['class' => $requestSingletonClass, 'closure' => $requestSingletonSupplier];
+        $this->requestSingletons[] = [
+            'class' => $requestSingletonClass,
+            'closure' => $requestSingletonSupplier,
+            'defer' => false
+        ];
+
+        $this->lastAddedSingletonType = 'request';
+
+        return $this;
     }
 
     protected function registerServerSingleton(string $serverSingletonClass, Closure $serverSingletonSupplier)
     {
-        $this->serverSingletons[] = ['class' => $serverSingletonClass, 'closure' => $serverSingletonSupplier];
+        $this->serverSingletons[] = [
+            'class' => $serverSingletonClass,
+            'closure' => $serverSingletonSupplier,
+            'defer' => false
+        ];
+
+        $this->lastAddedSingletonType = 'server';
+
+        return $this;
     }
 }
