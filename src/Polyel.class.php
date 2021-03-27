07a9c9db4df548e4e228331aa07fdab11e2a203c
embluk <embluk@tuta.io>
2021-03-27 14:16:00 +0000

    Update Polyel version to v0.6.3

--- src/Polyel.class.php
+++ src/Polyel.class.php
@@ -4,7 +4,7 @@ use Polyel\Container\Container;
 
 class Polyel
 {
-    private const version = '0.6.2';
+    private const version = '0.6.3';
 
     private static Polyel\Container\Container $container;
 
