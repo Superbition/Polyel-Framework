6988a162905224805ec4dc6cfd13de263071b6da
embluk <embluk@tuta.io>
2021-04-03 13:12:55 +0100

    Add the ability to set a email from name

--- src/Email/Email.class.php
+++ src/Email/Email.class.php
@@ -4,13 +4,24 @@ namespace Polyel\Email;
 
 abstract class Email
 {
+    protected string $fromName;
+
     protected string $subject;
 
     protected string $message;
 
+    abstract public function setFromName();
+
     abstract public function setSubject();
 
     abstract public function setMessage();
+
+    protected function name(string $fromName)
+    {
+        $this->fromName = $fromName;
+
+        return $this;
+    }
 
     protected function subject(string $subject)
     {
