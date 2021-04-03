b5c962284fb93c7223ff3325fca3ce982c1dd870
embluk <embluk@tuta.io>
2021-04-03 15:17:39 +0100

    Add an EmailManager to handle building and sending an SMTP email

---
+++ src/Email/EmailManager.class.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Polyel\Email;
+
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class EmailManager
+{
+    public function __construct()
+    {
+
+    }
+
+    private function createNewMailer()
+    {
+        $mailer = new PHPMailer(true);
+
+        $mailer->isSMTP();
+        $mailer->SMTPAuth = true;
+
+        return $mailer;
+    }
+
+    private function setMailerEncryption(string $from, $mailer)
+    {
+        //$mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+        $mailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+
+        return $mailer;
+    }
+
+    private function setSmtpHost(string $from, $mailer)
+    {
+        $mailer->Host = config("email.senders.$from.host");
+
+        return $mailer;
+    }
+
+    private function setSmtpPort(string $from, $mailer)
+    {
+        $mailer->Port = (int)config("email.senders.$from.port");
+
+        return $mailer;
+    }
+
+    private function setSmtpAuthCredentials(string $from, $mailer)
+    {
+        $mailer->Username = config("email.senders.$from.username");
+        $mailer->Password = config("email.senders.$from.password");
+
+        return $mailer;
+    }
+
+    public function send(string $to, string $from, Email $email)
+    {
+        if(empty($from))
+        {
+            $from = config('email.default');
+        }
+
+        $mailer = $this->createNewMailer();
+
+        $mailer = $this->setMailerEncryption($from, $mailer);
+        $mailer = $this->setSmtpHost($from, $mailer);
+        $mailer = $this->setSmtpPort($from, $mailer);
+        $mailer = $this->setSmtpAuthCredentials($from, $mailer);
+
+        $mailer->setFrom($mailer->Username, $email->fromName);
+
+        $mailer->addAddress($to);
+
+        $mailer->isHTML(true);
+
+        $mailer->Subject = $email->subject;
+
+        $mailer->Body = $email->message;
+
+        $mailer->send();
+    }
+}
