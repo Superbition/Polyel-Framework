41308065a8caf82cc8382fc7601a1d54a0fc1c81
Luke Embrey <embluk@tuta.io>
2021-12-08 13:02:52 +0000

    Update Polyel to 0.8.2 for new release

--- src/Polyel.class.php
+++ src/Polyel.class.php
@@ -4,7 +4,7 @@ use Polyel\Container\Container;
 
 class Polyel
 {
-    private const version = '0.8.1';
+    private const version = '0.8.2';
 
     // Used to access the HTTP server object
     private static $server;
